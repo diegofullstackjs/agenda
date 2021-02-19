d2d72905fe1cd6b0d4671b21977464436cc929d3
Kelvin Diego <kelvin.fabiana@gmail.com>
2021-02-19 14:39:46 +0000

    Adcionando Entidade 'UserEntity' UserServices

--- src/business/business.module.ts
+++ src/business/business.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import BusinessEntity from 'src/database/entities/Business.entity';
+import UserEntity from 'src/database/entities/Users.entity';
+import UsersServices from 'src/users/services/services';
 import BusinessController from './controllers/business.controller';
 import BusinesService from './services/business.services'
 @Module({
-    imports:[TypeOrmModule.forFeature([BusinessEntity])],
+    imports:[TypeOrmModule.forFeature([BusinessEntity,UserEntity])],
     controllers: [BusinessController],
-    providers:[BusinesService]
+    providers:[BusinesService,UsersServices]
 })
 export class BusinessModule {}
