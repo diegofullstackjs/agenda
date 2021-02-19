9afc7ad6a25b25fe4d15431ad6c5a786d30eead5
Kelvin Diego <kelvin.fabiana@gmail.com>
2021-02-19 14:37:10 +0000

    Adcionando modulo de autentificaçao

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,19 @@
+import { AuthenticationModule } from './authentication/authentication.module';
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { AdministratorModule } from './administrator/administrator.module';
 import { BusinessModule } from './business/business.module';
 
 @Module({
+
   imports: [
     DatabaseModule,
     UsersModule,
     AdministratorModule,
-    BusinessModule
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    BusinessModule,
+    AuthenticationModule,
+  ]
 })
-export class AppModule {}
+export class AppModule { }
